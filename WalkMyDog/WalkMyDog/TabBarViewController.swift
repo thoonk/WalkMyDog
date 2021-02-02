c2ebb9b670d3a6b57dc872aaea0a7b480524bc36
thoonk <dkrmrm878@gmail.com>
2021-02-02 16:35:04 +0900

    Add WalkMyDog with Firebase&Apple Login

---
+++ WalkMyDog/WalkMyDog/TabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarViewController.swift
+//  WalkMyDog
+//
+//  Created by 김태훈 on 2021/02/02.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.selectedIndex = 1
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
