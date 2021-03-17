069c5eaec637b2b3e7a01242c2c3972ed4c3466a
thoonk <dkrmrm878@gmail.com>
2021-03-17 23:13:31 +0900

    [Fix] move to separate group

---
+++ WalkMyDog/WalkMyDog/Scenes/Setting/PuppyTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PuppyTableViewCell.swift
+//  WalkMyDog
+//
+//  Created by 김태훈 on 2021/02/21.
+//
+
+import UIKit
+
+class PuppyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var puppyNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.selectionStyle = .none
+    }
+}
